043df3565f0b876a4f56dfa6871c5dc8b02a325e
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-01-16 13:30:46 +0100

    init(intial commit): Commit initial avec la structure du repository

---
+++ src/CouteauSuisse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CouteauSuisse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
